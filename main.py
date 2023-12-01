dc2b2e8441a9a218d97c014afe19d377438f4f27
ArtInLines <linesinart@gmail.com>
2023-12-01 14:54:08 +0100

    feat: adapted to Breadth-First-Search to reduce memory usage

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ from datetime import datetime
 from sys import argv
 dotenv.load_dotenv()
 
-default_degrees = 5
-default_start_acc = "Rex2002"
 print_req_info = True
 headers = {
 	"Authorization": "Bearer " + os.getenv("API_TOKEN"),
@@ -120,7 +118,7 @@ def get_json_list(endpoint: str, mapper = None, max_amount: int = None):
 def clean_db(db: Driver):
 	db.execute_query("MATCH (p) DETACH DELETE (p)")
 
-def get_and_sync_list(db: Driver, degrees: int, endpoint: str, key: str, max: int, query: str, query_key: str = None, **query_args):
+def get_and_sync_list(db: Driver, endpoint: str, key: str, max: int, query: str, query_key: str = None, **query_args):
 	data = get_json_list(endpoint, lambda x: x[key], max)
 	for x in data:
 		records = []
@@ -128,15 +126,12 @@ def get_and_sync_list(db: Driver, degrees: int, endpoint: str, key: str, max: in
 			query_args[query_key] = x
 		records, _, _ = db.execute_query(query, **query_args)
 		assert(len(records) == 1)
-		u = records[0].value()
-		if u['visited'] == 0:
-			search(u['name'], db, degrees - 1)
 
-def search(start_acc: str, db: Driver, degrees: int = 2):
+def search(start_acc: str, db: Driver):
 	records, _, _ = db.execute_query("""
 		MERGE (u:User {name: $uname})
-		ON CREATE SET u.visited=1
-		ON MATCH SET u.visited=u.visited+1
+		ON CREATE SET u.visited=0
+		ON MATCH SET u.visited=u.visited + toInteger(u.visited > 0)
 		RETURN u
 	""", uname=start_acc)
 	assert(len(records) == 1)
@@ -158,92 +153,101 @@ def search(start_acc: str, db: Driver, degrees: int = 2):
 		print("\033[33m[WARNING] Couldn't get data for user '" + start_acc + "'\033[0m")
 		return
 
-	if degrees > 0:
-		get_and_sync_list(db, degrees, f"users/{start_acc}/followers", "login", followers_count, """
-			MATCH (u1:User {name: $uname})
-			MERGE (u2:User {name: $followername})
-			ON CREATE SET u2.visited = 0
-			MERGE (u1)<-[:FOLLOWS]-(u2)
-			RETURN u2
-		""", "followername", uname=start_acc)
+	get_and_sync_list(db, f"users/{start_acc}/followers", "login", followers_count, """
+		MATCH (u1:User {name: $uname})
+		MERGE (u2:User {name: $followername})
+		ON CREATE SET u2.visited = 0
+		MERGE (u1)<-[:FOLLOWS]-(u2)
+		RETURN u2
+	""", "followername", uname=start_acc)
 
-		get_and_sync_list(db, degrees, f"users/{start_acc}/following", "login", following_count, """
-			MATCH (u1:User {name: $uname})
-			MERGE (u2:User {name: $followeename})
-			ON CREATE SET u2.visited = 0
-			MERGE (u1)-[:FOLLOWS]->(u2)
-			RETURN u2
-		""", "followeename", uname=start_acc)
+	get_and_sync_list(db, f"users/{start_acc}/following", "login", following_count, """
+		MATCH (u1:User {name: $uname})
+		MERGE (u2:User {name: $followeename})
+		ON CREATE SET u2.visited = 0
+		MERGE (u1)-[:FOLLOWS]->(u2)
+		RETURN u2
+	""", "followeename", uname=start_acc)
 
-		repos = get_json_list(f"users/{start_acc}/repos", lambda x: x["full_name"])
-		for repo in repos:
-			records, _, _ = db.execute_query("""
-				MATCH (u:User {name: $uname})
-				MERGE (r:Repo {name: $rname})
-				ON CREATE SET r.visited=1
-				ON MATCH SET r.visited=r.visited+1
-				MERGE (u)-[:OWNS]->(r)
-				RETURN r
-			""", uname=start_acc, rname=repo)
-			assert(len(records) == 1)
-			r = records[0].value()
-			if r['visited'] <= 1:
-				forks = get_json_list(f"repos/{repo}/forks", lambda x: x["full_name"])
-				for fork in forks:
-					records, _, _ = db.execute_query("""
-						MERGE (r:Repo {name: $rname})
-						ON CREATE SET r.visited=0
-						RETURN r
-					""", rname=fork)
-					assert(len(records) == 1)
-					r = records[0].value()
-					records, _, _ = db.execute_query("""
-						MATCH (r1:Repo {name: $r1name})
-						MATCH (r2:Repo {name: $r2name})
-						CREATE (r1)-[rel:FORKED_TO]->(r2)
-					""", r1name=repo, r2name=fork)
-					if r['visited'] == 0:
-						db.execute_query("""
-							MATCH (r:Repo {name: $rname})
-							MATCH (u:User {name: $uname})
-							CREATE (u)-[:OWNS]->(r)
-						""", rname=fork, uname=fork.split("/")[0])
-						search(fork.split("/")[0], db, degrees-1)
+	repos = get_json_list(f"users/{start_acc}/repos", lambda x: x["full_name"])
+	for repo in repos:
+		records, _, _ = db.execute_query("""
+			MATCH (u:User {name: $uname})
+			MERGE (r:Repo {name: $rname})
+			ON CREATE SET r.visited=1
+			ON MATCH SET r.visited=r.visited+1
+			MERGE (u)-[:OWNS]->(r)
+			RETURN r
+		""", uname=start_acc, rname=repo)
+		assert(len(records) == 1)
+		r = records[0].value()
+		if r['visited'] <= 1:
+			forks = get_json_list(f"repos/{repo}/forks", lambda x: x["full_name"])
+			for fork in forks:
+				records, _, _ = db.execute_query("""
+					MERGE (r:Repo {name: $rname})
+					ON CREATE SET r.visited=0
+					RETURN r
+				""", rname=fork)
+				assert(len(records) == 1)
+				r = records[0].value()
+				records, _, _ = db.execute_query("""
+					MATCH (r1:Repo {name: $r1name})
+					MATCH (r2:Repo {name: $r2name})
+					MERGE (r1)-[rel:FORKED_TO]->(r2)
+				""", r1name=repo, r2name=fork)
+				if r['visited'] == 0:
+					db.execute_query("""
+						MATCH (r:Repo {name: $rname})
+						MATCH (u:User {name: $uname})
+						MERGE (u)-[:OWNS]->(r)
+					""", rname=fork, uname=fork.split("/")[0])
 
-				get_and_sync_list(db, degrees, f"repos/{repo}/contributors", "login", None, """
-					MATCH (r:Repo {name: $rname})
-					MERGE (u:User {name: $uname})
-					ON CREATE SET u.visited = 0
-					CREATE (u)-[:CONTRIBUTED]->(r)
-					RETURN u
-				""", "uname", rname=repo)
+			get_and_sync_list(db, f"repos/{repo}/contributors", "login", None, """
+				MATCH (r:Repo {name: $rname})
+				MERGE (u:User {name: $uname})
+				ON CREATE SET u.visited = 0
+				MERGE (u)-[:CONTRIBUTED]->(r)
+				RETURN u
+			""", "uname", rname=repo)
 
-				get_and_sync_list(db, degrees, f"repos/{repo}/stargazers", "login", None, """
-					MATCH (r:Repo {name: $rname})
-					MERGE (u:User {name: $uname})
-					ON CREATE SET u.visited = 0
-					CREATE (u)-[:STARRED]->(r)
-					RETURN u
-				""", "uname", rname=repo)
+			get_and_sync_list(db, f"repos/{repo}/stargazers", "login", None, """
+				MATCH (r:Repo {name: $rname})
+				MERGE (u:User {name: $uname})
+				ON CREATE SET u.visited = 0
+				MERGE (u)-[:STARRED]->(r)
+				RETURN u
+			""", "uname", rname=repo)
+	records, _, _ = db.execute_query("""
+		MATCH (u:User {name: $uname})
+		SET u.visited = 1
+	""", uname=start_acc)
 
 if __name__ == "__main__":
-	if len(argv) != 3 and len(argv) != 4:
+	if len(argv) != 2 and len(argv) != 3:
 		print("\033[31mInvalid Amount of arguments\033[0m")
 		print("Usage:")
-		print(f"> python {argv[0]} <Account to start Search From> <Max Degrees for Search> [-c]")
+		print(f"> python {argv[0]} <Account to start Search from> [-c]")
 		print("The optional '-c' flag cleans the database before starting the script")
 		print("")
 		print("Example:")
-		print(f"> python {argv[0]} {default_start_acc} {default_degrees}")
+		print(f"> python {argv[0]} ArtInLines")
 		os._exit(1)
 	start_acc = argv[1]
-	degrees   = int(argv[2])
 
 	db = GraphDatabase.driver(os.getenv("DB_URI"), auth=(os.getenv("DB_USER"), os.getenv("DB_PASS")))
 	db.verify_connectivity()
-	if len(argv) == 4 and argv[3].startswith("-c"):
+	if len(argv) == 3 and argv[2].startswith("-c"):
 		clean_db(db)
 	db.execute_query("CREATE CONSTRAINT IF NOT EXISTS FOR (x:User) REQUIRE x.name IS UNIQUE")
 	db.execute_query("CREATE CONSTRAINT IF NOT EXISTS FOR (x:Repo) REQUIRE x.name IS UNIQUE")
-	search(start_acc, db, degrees)
+
+	acc = start_acc
+	while True:
+		search(acc, db)
+		records, _, _ = db.execute_query("""MATCH (u:User {visited: 0}) RETURN u.name LIMIT 1""")
+		if len(records) == 0:
+			break
+		acc = records[0].value()
+
 	db.close()
